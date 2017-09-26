e04986c3550367ec3c59f0ad41aebea36fca62ae
Charles Jonas <charlie@callawaycloudconsulting.com>
2017-09-25 23:50:01 -0600

    made test more realistic

--- tests/Example.spec.tsx
+++ tests/Example.spec.tsx
@@ -3,24 +3,23 @@ import * as React from "react";
 import {Counter} from "../src/components/counter";
 import {TodoItem} from "../src/components/todoItem";
 
-test("Checkbox has things", () => {
-  // Render a checkbox with label in the document
-  const counter = shallow(
-    <Counter />,
-  );
-
-  expect(counter.find("h3").text()).toEqual("Behold the mighty counter!");
-
-  counter.find("Slider").simulate("change");
-  const progress = counter.find("Progress");
-  expect(progress).toHaveLength(2);
-  // expect(counter.text()).toEqual("On");
-});
-
-it("allows us to set props", () => {
+it("test todo item button", () => {
+  const mockCallback = jest.fn();
   // Use the method 'mount' to perform a full render
-  const foo = mount(<TodoItem id="123" icon="123" text="123" key="123" onClick={() => {}} iconColor="123" />);
-  expect(foo.prop("id")).toBe("123");
-  foo.setProps({ id: "foo" });
-  expect(foo.prop("id")).toBe("foo");
+  const todoItem = mount((
+    <TodoItem
+      id="123"
+      icon="check"
+      text="todo"
+      key="123"
+      iconColor="green"
+      onClick={mockCallback}
+    />
+  ));
+
+  const btn = todoItem.find("Button");
+  expect(btn.prop("loading")).toEqual(false);
+  btn.simulate("click");
+  expect(mockCallback.mock.calls.length).toBe(1);
+  expect(btn.prop("loading")).toEqual(true);
 });
