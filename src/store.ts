d001bf8a9a7ddf23ec2bc6163d2363cf01f2ab05
Charles Jonas <charlie@callawaycloudconsulting.com>
2017-09-25 22:45:50 -0600

    added react-dev-tools

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,11 @@
 import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import reducer, {GlobalState} from "./reducers/index";
 
 const initState: GlobalState = {todo: [], done: []};
 const createGlobalStore = () => {
-  return createStore<GlobalState>(reducer, initState, applyMiddleware(thunk));
+  return createStore<GlobalState>(reducer, initState, composeWithDevTools(applyMiddleware(thunk)));
 };
 
 export default createGlobalStore;
