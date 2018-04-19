a7dde90e745ca77ef0d93d199f800fc723e98727
Charles Jonas <charlie@callawaycloudconsulting.com>
2018-04-19 17:47:04 -0600

    fixing unit test

--- src/tests/Example.spec.tsx
+++ src/tests/Example.spec.tsx
@@ -1,4 +1,4 @@
-import {TodoItem} from "@src/components/shared/todoItem";
+import App from "@src/app"
 import {configure, mount} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import * as React from "react";
@@ -9,21 +9,9 @@ it("test todo item button", () => {
   const mockCallback = jest.fn();
   // Use the method 'mount' to perform a full render
   const todoItem = mount((
-    <TodoItem
-      id="123"
-      icon="check"
-      text="todo"
-      key="123"
-      iconColor="green"
-      onClick={mockCallback}
-    />
+    <App />
   ));
 
-  let btn = todoItem.find("Button");
-  expect(btn.prop("loading")).toEqual(false);
-  btn.simulate("click");
-  todoItem.update();
-  btn = todoItem.find("Button");
-  expect(mockCallback.mock.calls.length).toBe(1);
-  expect(btn.prop("loading")).toEqual(true);
+  const btn = todoItem.find('Card');
+  expect(btn.prop('title')).toEqual('B.A.S.S. Blank!');
 });
