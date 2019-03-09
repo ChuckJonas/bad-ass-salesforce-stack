0a53163b725f6adc40ecab45dc40dcc8d196533a
Charles Jonas <charlie@callawaycloudconsulting.com>
2019-03-09 00:43:40 -0700

    fixing unit tests

--- src/tests/Example.spec.tsx
+++ src/tests/Example.spec.tsx
@@ -1,17 +1,16 @@
-import App from "@src/app"
-import {configure, mount} from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import * as React from "react";
+import App from '@src/app';
+import {configure, mount} from 'enzyme';
+import * as React from 'react';
+import * as Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-it("test todo item button", () => {
-  const mockCallback = jest.fn();
+it('test todo item button', () => {
   // Use the method 'mount' to perform a full render
   const todoItem = mount((
     <App />
   ));
 
   const btn = todoItem.find('Card');
-  expect(btn.prop('title')).toEqual('B.A.S.S. Blank!');
+  expect(btn.prop('title')).toEqual('B.A.S.S.');
 });
